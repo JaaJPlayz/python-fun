9f609ccd8600d92b8e501f00f83a0e5e7eb2565a
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-06-19 22:18:02 -0300

    Feat: Made a password generator lol

---
+++ src/index.py
@@ -0,0 +1,57 @@
+from random import choice
+
+
+class PasswordGenerator:
+    ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+    ALPHABET_UPPERCASE = ALPHABET.upper()
+    NUMBERS = "0123456789"
+    SPECIAL_CHARACTERS = "!@#$%^&*()"
+
+    def __init__(self):
+        pass
+
+    def generate(self, pass_length):
+        resulting_password = ""
+
+        for _ in range(pass_length):
+            random_letter = choice(self.ALPHABET)
+            random_letter_uppercase = choice(self.ALPHABET_UPPERCASE)
+            random_number = choice(self.NUMBERS)
+            random_special_character = choice(self.SPECIAL_CHARACTERS)
+
+            resulting_password += choice(
+                [
+                    random_letter,
+                    random_number,
+                    random_special_character,
+                    random_letter_uppercase,
+                ]
+            )
+
+        return resulting_password
+
+    def validate(self, password):
+        valid_chars = (
+            self.ALPHABET
+            + self.ALPHABET_UPPERCASE
+            + self.NUMBERS
+            + self.SPECIAL_CHARACTERS
+        )
+        for char in password:
+            if char not in valid_chars:
+                return False
+
+        return True
+
+
+pg = PasswordGenerator()
+
+
+for i in range(10):
+    if i == 5:
+        new_pass = pg.generate(10) + r"\\\\\\"
+        print(new_pass, pg.validate(new_pass))
+        continue
+    new_pass = pg.generate(16)
+    pass_val = pg.validate(new_pass)
+    print(new_pass, pass_val)
